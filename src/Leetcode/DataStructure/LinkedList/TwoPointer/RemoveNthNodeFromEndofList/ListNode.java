16643ef34a251bded0d4b0be762d749af2c7301e
R7trztrz <R7trztrz@outlook.com>
2026-06-04 17:57:54 +1000

    feat(LinkedList/TwoPointer): add solution of RemoveNthNodeFromEndofList (#32)

---
+++ src/Leetcode/DataStructure/LinkedList/TwoPointer/RemoveNthNodeFromEndofList/ListNode.java
@@ -0,0 +1,9 @@
+package Leetcode.DataStructure.LinkedList.TwoPointer.RemoveNthNodeFromEndofList;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
