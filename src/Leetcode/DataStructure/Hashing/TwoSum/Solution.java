34b0921cac03349759da2adedccb3d3357a9b5ac
R7trztrz <R7trztrz@outlook.com>
2026-06-24 03:32:10 +1000

    feat(Hashing): add solution of TwoSun

---
+++ src/Leetcode/DataStructure/Hashing/TwoSum/Solution.java
@@ -0,0 +1,23 @@
+package Leetcode.DataStructure.Hashing.TwoSum;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @since 24 / 06 / 2026
+ */
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (hashMap.containsKey(target - nums[i])) {
+                return new int[]{hashMap.get(target - nums[i]), i};
+            }
+
+            hashMap.put(nums[i], i);
+        }
+
+        return null;
+    }
+}
